4df4584ce378e1e12ca9a1ad0c0b2a3e2002f772
sachin <sachin.singh@manektech.com>
2023-02-17 15:36:26 +0530

    design model & middleware

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ app.use(errorHandler)
 app.use(cors());
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
